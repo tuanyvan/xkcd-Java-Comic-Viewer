ddd3f39a9b9f48ae83f0b0072dcf9a96333638ea
tuanyvan <93796037+tuanyvan@users.noreply.github.com>
2022-03-27 23:06:16 -0400

    Create prototype for xkcd HTTPS request
    Imported org.json Maven dependency and created original implementation to fetch JSON responses from HTTPS requests

--- src/main/java/com/tuanyvan/xkcdjavacomicviewer/xkcdController.java
+++ src/main/java/com/tuanyvan/xkcdjavacomicviewer/xkcdController.java
@@ -1,14 +1,50 @@
 package com.tuanyvan.xkcdjavacomicviewer;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 
-public class xkcdController {
-    @FXML
-    private Label welcomeText;
+import javax.net.ssl.HttpsURLConnection;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.util.ResourceBundle;
+import org.json.JSONObject;
+
+public class xkcdController implements Initializable {
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private ImageView comicImageView;
+
+    @FXML
+    private Label comicInformationLabel;
+
+    private int newestComicId;
+
+    @FXML
+    void generateRandomComicButton(ActionEvent event) {
+
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        try {
+            // Establish a connection to the most recent comic JSON.
+            HttpsURLConnection request = (HttpsURLConnection) new URL("https://xkcd.com/info.0.json").openConnection();
+            request.setRequestProperty("User-Agent", "application/json");
+            InputStream response = request.getInputStream();
+
+            // Turn the response into a JSON object.
+            JSONObject json = new JSONObject(new String(response.readAllBytes()));
+
+            int newestComicId = (int) json.get("num");
+
+            comicInformationLabel.setText("The newest comic is #" + newestComicId + ".");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.tuanyvan.xkcdjavacomicviewer {
     requires javafx.controls;
     requires javafx.fxml;
+    requires org.json;
 
 
     opens com.tuanyvan.xkcdjavacomicviewer to javafx.fxml;
