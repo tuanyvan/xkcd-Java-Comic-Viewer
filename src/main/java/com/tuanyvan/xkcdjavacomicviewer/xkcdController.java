eae170fdbdf9ee049c4cc6a6cff7b7753a682c93
tuanyvan <93796037+tuanyvan@users.noreply.github.com>
2022-04-03 15:48:37 -0400

    Add rest of metadata into view

--- src/main/java/com/tuanyvan/xkcdjavacomicviewer/xkcdController.java
+++ src/main/java/com/tuanyvan/xkcdjavacomicviewer/xkcdController.java
@@ -19,10 +19,19 @@ import org.json.JSONObject;
 public class xkcdController implements Initializable {
 
     @FXML
+    private Label altLabel;
+
+    @FXML
     private ImageView comicImageView;
 
     @FXML
-    private Label comicInformationLabel;
+    private Label comicIdLabel;
+
+    @FXML
+    private Label dateCreatedLabel;
+
+    @FXML
+    private Label titleLabel;
 
     private int newestComicId;
 
@@ -31,21 +40,38 @@ public class xkcdController implements Initializable {
 
     }
 
+    private Comic makeComicRequest(String url) throws IOException {
+        // Establish a connection to the most recent comic JSON.
+        HttpsURLConnection request = (HttpsURLConnection) new URL(url).openConnection();
+        request.setRequestProperty("User-Agent", "application/json");
+        InputStream response = request.getInputStream();
+
+        // Turn the response into a JSON object.
+        JSONObject json = new JSONObject(new String(response.readAllBytes()));
+
+        return new Comic(json);
+    }
+
+    private void setControllerLabels(Comic comic) {
+        comicIdLabel.setText("#" + comic.getComicID());
+        titleLabel.setText(comic.getTitle());
+        altLabel.setText(comic.getAltText());
+        dateCreatedLabel.setText(
+                String.format(
+                        "%s (%s)",
+                        comic.getPublishedDate().toString(),
+                        comic.getTimeSincePublication()
+                )
+        );
+        comicImageView.setImage(new Image(comic.getImgURI()));
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         try {
-            // Establish a connection to the most recent comic JSON.
-            HttpsURLConnection request = (HttpsURLConnection) new URL("https://xkcd.com/info.0.json").openConnection();
-            request.setRequestProperty("User-Agent", "application/json");
-            InputStream response = request.getInputStream();
-
-            // Turn the response into a JSON object.
-            JSONObject json = new JSONObject(new String(response.readAllBytes()));
-            Comic newestComic = new Comic(json);
-            newestComicId = newestComic.getComicID();
-            comicInformationLabel.setText("The newest comic is #" + newestComicId + ".");
-            comicImageView.setImage(new Image(newestComic.getImgURI()));
-
+            Comic newestComic = makeComicRequest("https://xkcd.com/info.0.json");
+            newestComicId = newestComic.getComicID(); // Track the upper bound for the random comic generator
+            setControllerLabels(newestComic);
         } catch (IOException e) {
             e.printStackTrace();
         }
